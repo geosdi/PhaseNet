5b6e81d94577848cfa3cdc757cdf2d8e1559aaeb
Francesco Izzi <francescoizzi@MacMiniDiChiara2024.local>
2024-04-11 19:55:27 +0200

    Try to distribute trainigin on GPU

--- phasenet/predict.py
+++ phasenet/predict.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import multiprocessing
 import os
 import pickle
 import time
@@ -76,153 +75,106 @@ def pred_fn(args, data_reader, figure_dir=None, prob_dir=None, log_dir=None):
     logging.info("Pred log: %s" % log_dir)
     logging.info("Dataset size: {}".format(data_reader.num_data))
 
-    with tf.compat.v1.name_scope("Input_Batch"):
-        if args.format == "mseed_array":
-            batch_size = 1
-        else:
-            batch_size = args.batch_size
-        dataset = data_reader.dataset(batch_size)
-        batch = tf.compat.v1.data.make_one_shot_iterator(dataset).get_next()
+    strategy = tf.distribute.MirroredStrategy()
 
-    config = ModelConfig(X_shape=data_reader.X_shape)
-    with open(os.path.join(log_dir, "config.log"), "w") as fp:
-        fp.write("\n".join("%s: %s" % item for item in vars(config).items()))
-
-    model = UNet(config=config, input_batch=batch, mode="pred")
-    # model = UNet(config=config, mode="pred")
-    sess_config = tf.compat.v1.ConfigProto()
-    sess_config.gpu_options.allow_growth = True
-    # sess_config.log_device_placement = False
-
-    with tf.compat.v1.Session(config=sess_config) as sess:
-        saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables(), max_to_keep=5)
-        init = tf.compat.v1.global_variables_initializer()
-        sess.run(init)
-
-        latest_check_point = tf.train.latest_checkpoint(args.model_dir)
-        logging.info(f"restoring model {latest_check_point}")
-        saver.restore(sess, latest_check_point)
-
-        picks = []
-        amps = [] if args.amplitude else None
-        if args.plot_figure:
-            multiprocessing.set_start_method("spawn")
-            pool = multiprocessing.Pool(multiprocessing.cpu_count())
-
-        for _ in tqdm(range(0, data_reader.num_data, batch_size), desc="Pred"):
-            if args.amplitude:
-                pred_batch, X_batch, amp_batch, fname_batch, t0_batch, station_batch = sess.run(
-                    [model.preds, batch[0], batch[1], batch[2], batch[3], batch[4]],
-                    feed_dict={model.drop_rate: 0, model.is_training: False},
-                )
-            #    X_batch, amp_batch, fname_batch, t0_batch = sess.run([batch[0], batch[1], batch[2], batch[3]])
+    with strategy.scope():
+        with tf.compat.v1.name_scope("Input_Batch"):
+            if args.format == "mseed_array":
+                batch_size = 1
             else:
-                pred_batch, X_batch, fname_batch, t0_batch, station_batch = sess.run(
-                    [model.preds, batch[0], batch[1], batch[2], batch[3]],
-                    feed_dict={model.drop_rate: 0, model.is_training: False},
-                )
-            #    X_batch, fname_batch, t0_batch = sess.run([model.preds, batch[0], batch[1], batch[2]])
-            # pred_batch = []
-            # for i in range(0, len(X_batch), 1):
-            #     pred_batch.append(sess.run(model.preds, feed_dict={model.X: X_batch[i:i+1], model.drop_rate: 0, model.is_training: False}))
-            # pred_batch = np.vstack(pred_batch)
+                batch_size = args.batch_size
+            dataset = data_reader.dataset(batch_size)
+            batch = tf.compat.v1.data.make_one_shot_iterator(dataset).get_next()
 
-            waveforms = None
-            if args.amplitude:
-                waveforms = amp_batch
+        config = ModelConfig(X_shape=data_reader.X_shape)
+        with open(os.path.join(log_dir, "config.log"), "w") as fp:
+            fp.write("\n".join("%s: %s" % item for item in vars(config).items()))
 
-            picks_ = extract_picks(
-                preds=pred_batch,
-                file_names=fname_batch,
-                station_ids=station_batch,
-                begin_times=t0_batch,
-                config=args,
-                waveforms=waveforms,
-                use_amplitude=args.amplitude,
-                dt=1.0 / args.sampling_rate,
-            )
+        model = UNet(config=config, input_batch=batch, mode="pred")
+        sess_config = tf.compat.v1.ConfigProto()
+        sess_config.gpu_options.allow_growth = True
 
-            picks.extend(picks_)
+        with tf.compat.v1.Session(config=sess_config) as sess:
+            saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables(), max_to_keep=5)
+            init = tf.compat.v1.global_variables_initializer()
+            sess.run(init)
 
-            # ## save pick per file
-            # if (len(fname_batch) == 1) & (len(picks_) > 0):
-            #     df = pd.DataFrame(picks_)
-            #     df = df[df["phase_index"] > 10]
-            #     if not os.path.exists(os.path.join(args.result_dir, "picks")):
-            #         os.makedirs(os.path.join(args.result_dir, "picks"))
-            #     df = df[
-            #         [
-            #             "station_id",
-            #             "begin_time",
-            #             "phase_index",
-            #             "phase_time",
-            #             "phase_score",
-            #             "phase_type",
-            #             "phase_amplitude",
-            #             "dt",
-            #         ]
-            #     ]
-            #     df.to_csv(
-            #         os.path.join(
-            #             args.result_dir, "picks", fname_batch[0].decode().split("/")[-1].rstrip(".mseed") + ".csv"
-            #         ),
-            #         index=False,
-            #     )
+            latest_check_point = tf.train.latest_checkpoint(args.model_dir)
+            logging.info(f"restoring model {latest_check_point}")
+            saver.restore(sess, latest_check_point)
 
+            picks = []
+            amps = [] if args.amplitude else None
             if args.plot_figure:
-                if not (isinstance(fname_batch, np.ndarray) or isinstance(fname_batch, list)):
-                    fname_batch = [fname_batch.decode().rstrip(".mseed") + "_" + x.decode() for x in station_batch]
-                else:
-                    fname_batch = [x.decode() for x in fname_batch]
-                pool.starmap(
-                    partial(
-                        plot_waveform,
-                        figure_dir=figure_dir,
-                    ),
-                    # zip(X_batch, pred_batch, [x.decode() for x in fname_batch]),
-                    zip(X_batch, pred_batch, fname_batch),
+                for _ in tqdm(range(0, data_reader.num_data, batch_size), desc="Pred"):
+                    if args.amplitude:
+                        pred_batch, X_batch, amp_batch, fname_batch, t0_batch, station_batch = sess.run(
+                            [model.preds, batch[0], batch[1], batch[2], batch[3], batch[4]],
+                            feed_dict={model.drop_rate: 0, model.is_training: False},
+                        )
+                    else:
+                        pred_batch, X_batch, fname_batch, t0_batch, station_batch = sess.run(
+                            [model.preds, batch[0], batch[1], batch[2], batch[3]],
+                            feed_dict={model.drop_rate: 0, model.is_training: False},
+                        )
+
+                    waveforms = None
+                    if args.amplitude:
+                        waveforms = amp_batch
+
+                    picks_ = extract_picks(
+                        preds=pred_batch,
+                        file_names=fname_batch,
+                        station_ids=station_batch,
+                        begin_times=t0_batch,
+                        config=args,
+                        waveforms=waveforms,
+                        use_amplitude=args.amplitude,
+                        dt=1.0 / args.sampling_rate,
+                    )
+
+                    picks.extend(picks_)
+
+                    if args.plot_figure:
+                        if not (isinstance(fname_batch, np.ndarray) or isinstance(fname_batch, list)):
+                            fname_batch = [fname_batch.decode().rstrip(".mseed") + "_" + x.decode() for x in station_batch]
+                        else:
+                            fname_batch = [x.decode() for x in fname_batch]
+                        for i in range(len(X_batch)):
+                            plot_waveform(
+                                X_batch[i], pred_batch[i], fname_batch[i], figure_dir=figure_dir
+                            )
+
+                    if args.save_prob:
+                        if not (isinstance(fname_batch, np.ndarray) or isinstance(fname_batch, list)):
+                            fname_batch = [fname_batch.decode().rstrip(".mseed") + "_" + x.decode() for x in station_batch]
+                        else:
+                            fname_batch = [x.decode() for x in fname_batch]
+                        save_prob_h5(pred_batch, fname_batch, prob_h5)
+
+            if len(picks) > 0:
+                df = pd.DataFrame(picks)
+                base_columns = [
+                    "station_id",
+                    "begin_time",
+                    "phase_index",
+                    "phase_time",
+                    "phase_score",
+                    "phase_type",
+                    "file_name",
+                ]
+                if args.amplitude:
+                    base_columns.append("phase_amplitude")
+                    base_columns.append("phase_amp")
+                    df["phase_amp"] = df["phase_amplitude"]
+                df = df[base_columns]
+                df.to_csv(os.path.join(args.result_dir, args.result_fname + ".csv"), index=False)
+
+                print(
+                    f"Done with {len(df[df['phase_type'] == 'P'])} P-picks and {len(df[df['phase_type'] == 'S'])} S-picks"
                 )
-
-            if args.save_prob:
-                # save_prob(pred_batch, fname_batch, prob_dir=prob_dir)
-                if not (isinstance(fname_batch, np.ndarray) or isinstance(fname_batch, list)):
-                    fname_batch = [fname_batch.decode().rstrip(".mseed") + "_" + x.decode() for x in station_batch]
-                else:
-                    fname_batch = [x.decode() for x in fname_batch]
-                save_prob_h5(pred_batch, fname_batch, prob_h5)
-
-        if len(picks) > 0:
-            # save_picks(picks, args.result_dir, amps=amps, fname=args.result_fname+".csv")
-            # save_picks_json(picks, args.result_dir, dt=data_reader.dt, amps=amps, fname=args.result_fname+".json")
-            df = pd.DataFrame(picks)
-            # df["fname"] = df["file_name"]
-            # df["id"] = df["station_id"]
-            # df["timestamp"] = df["phase_time"]
-            # df["prob"] = df["phase_prob"]
-            # df["type"] = df["phase_type"]
-
-            base_columns = [
-                "station_id",
-                "begin_time",
-                "phase_index",
-                "phase_time",
-                "phase_score",
-                "phase_type",
-                "file_name",
-            ]
-            if args.amplitude:
-                base_columns.append("phase_amplitude")
-                base_columns.append("phase_amp")
-                df["phase_amp"] = df["phase_amplitude"]
-
-            df = df[base_columns]
-            df.to_csv(os.path.join(args.result_dir, args.result_fname + ".csv"), index=False)
-
-            print(
-                f"Done with {len(df[df['phase_type'] == 'P'])} P-picks and {len(df[df['phase_type'] == 'S'])} S-picks"
-            )
-        else:
-            print(f"Done with 0 P-picks and 0 S-picks")
+            else:
+                print(f"Done with 0 P-picks and 0 S-picks")
     return 0
 
 
